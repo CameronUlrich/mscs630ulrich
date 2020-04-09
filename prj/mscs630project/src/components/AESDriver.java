b2ac8af2ea51d5b718266ba220e471555aea84ea
CameronUlrich <Cameron.Ulrich1@marist.edu>
2020-04-09 14:19:36 -0400

    Working on encrypt button backend

---
+++ prj/mscs630project/src/components/AESDriver.java
@@ -0,0 +1,21 @@
+/*
+public class AESDriver {
+    /*
+     * main method:
+     * 
+     * Reads the system key, Ke, and plaintext block p from standard input, i.e., System.in
+     
+    public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+  
+      String key = input.nextLine();
+      
+      String plainText = input.nextLine();
+      
+      String cipherText = AEScipher.AES(plainText, key);
+      System.out.println(cipherText);
+  
+    }
+  
+  }
+  */
--- prj/mscs630project/src/components/EncryptPage.jsx
+++ prj/mscs630project/src/components/EncryptPage.jsx
@@ -5,7 +5,48 @@ import './styles/Encrypt.css';
 
 
 
+
 class EncryptPage extends Component {
+
+    encryptText = () => {
+        let textToEncrypt = document.getElementById('encryptTextInput');
+        
+        var aesjs = require("aes-js")
+        // the AES block cipher algorithm works on 16 byte bloca ks, no more, no less
+        var text = textToEncrypt.value;
+        var textAsBytes = aesjs.utils.utf8.toBytes(text)
+        console.log(textAsBytes);
+        // [65, 66, 108, 111, 99, 107, 73, 115, 49, 54, 66, 121, 116, 101, 115, 33]
+
+        // create an instance of the block cipher algorithm
+        var key = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3];
+        var aes = new aesjs.AES(key);
+
+        // encrypt...
+        var encryptedBytes = aes.encrypt(textAsBytes);
+        console.log(encryptedBytes);
+        // [136, 15, 199, 174, 118, 133, 233, 177, 143, 47, 42, 211, 96, 55, 107, 109] 
+
+        // To print or store the binary data, you may convert it to hex
+        var encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
+        console.log(encryptedHex);
+        // "880fc7ae7685e9b18f2f2ad360376b6d"
+
+        // When ready to decrypt the hex string, convert it back to bytes
+        var encryptedBytes = aesjs.utils.hex.toBytes(encryptedHex);
+
+        // decrypt...
+        var decryptedBytes = aes.decrypt(encryptedBytes);
+        console.log(decryptedBytes);
+        // [65, 66, 108, 111, 99, 107, 73, 115, 49, 54, 66, 121, 116, 101, 115, 33]
+
+
+        // decode the bytes back into our original text
+        var decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
+        console.log(decryptedText);
+
+    }
+
     render() {
         return(
             <div>
@@ -19,7 +60,7 @@ class EncryptPage extends Component {
 
                 <input id="encryptImageInput" type="file"></input>
 
-                <Button id="encryptButton">Encrypt</Button>
+                <Button id="encryptButton" onClick={this.encryptText}>Encrypt</Button>
             </div>
         );
         
