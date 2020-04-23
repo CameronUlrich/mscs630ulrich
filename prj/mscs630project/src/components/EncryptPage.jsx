f1c74fef6d66230de159ca5e8f4afc2cf7329fe7
CameronUlrich <Cameron.Ulrich1@marist.edu>
2020-04-23 15:00:55 -0400

    Working on getting image to contain hidden message

--- prj/mscs630project/src/components/EncryptPage.jsx
+++ prj/mscs630project/src/components/EncryptPage.jsx
@@ -4,14 +4,92 @@ import Header from './Header';
 import './styles/Encrypt.css';
 import { AES } from "crypto-js/aes";
 
+    
+    //imageLoader.addEventListener('change', handleImage, false);
+    /*
+    var canvas = document.getElementById('imageCanvas');
+    var ctx = canvas.getContext('2d');
+    var messageInput = document.getElementById('encryptTextInput');
 
+    var textCanvas = document.getElementById('textCanvas');
+    var tctx = textCanvas.getContext('2d');
+    */
 
+    //handle decoding
+    //var decodeCanvas = document.getElementById('imageCanvas2');
+    //var dctx = decodeCanvas.getContext('2d');
+    //var imageLoader2 = document.getElementById('imageLoader2');
+    //imageLoader2.addEventListener('change', handleImage2, false);
+
+    
+    
 
 class EncryptPage extends Component {
+    
+    handleImage = () => {
+        var imageLoader = document.getElementById('imageLoader');
+        imageLoader.addEventListener('change', handleImage, false);
+        var canvas = document.getElementById('imageCanvas');
+        var ctx = canvas.getContext('2d');
+        var messageInput = document.getElementById('encryptedLabel');
+
+        var textCanvas = document.getElementById('textCanvas');
+        var tctx = textCanvas.getContext('2d');
+        function handleImage(e){
+            var reader = new FileReader();
+            reader.onload = function(event){
+                var img = new Image();
+                img.onload = function(){
+                    canvas.width = img.width;
+                    canvas.height = img.height;
+                    textCanvas.width=img.width;
+                    textCanvas.height=img.height;
+                    tctx.font = "30px Arial";
+              var messageText = (messageInput.value.length) ? messageInput.value : 'Hello';
+                    tctx.fillText(messageText,10,50);
+                    ctx.drawImage(img,0,0);
+                    var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+                    var textData = tctx.getImageData(0, 0, canvas.width, canvas.height);
+                    var pixelsInMsg = 0;
+                     var pixelsOutMsg = 0;
+                    for (var i = 0; i < textData.data.length; i += 4) {
+                        if (textData.data[i+3] !== 0) {
+                            if (imgData.data[i+1]%10 == 7) {
+                                //do nothing, we're good
+                            }
+                            else if (imgData.data[i+1] > 247) {
+                                imgData.data[i+1] = 247;
+                            }
+                            else {
+                                while (imgData.data[i+1] % 10 != 7) {
+                                    imgData.data[i+1]++;
+                                }
+                            }
+                            pixelsInMsg++;
+                        }
+                        else {
+                            if (imgData.data[i+1]%10 == 7) {
+                                imgData.data[i+1]--;
+                            }
+                            pixelsOutMsg++;
+                        }
+                    }
+                    console.log('pixels within message borders: '+pixelsInMsg);
+                    console.log('pixels outside of message borders: '+pixelsOutMsg);
+                    ctx.putImageData(imgData, 0, 0);
+                };
+                img.src = event.target.result;
+            };
+            reader.readAsDataURL(e.target.files[0]);
+        }
+    }
+
+    
 
     encryptText = () => {
-        let textToEncrypt = document.getElementById('encryptTextInput');
+        let textToEncrypt = document.getElementById('message');
         let encryptKey = document.getElementById('encryptKeyInput');
+        let encryptedLabel = document.getElementById('encryptedLabel');
         
         var aesjs = require("aes-js");
         var AES = require("crypto-js/aes");
@@ -28,6 +106,7 @@ class EncryptPage extends Component {
         
         
         var key = keyArray;
+    
 
         // Convert text to bytes
         var text = textToEncrypt.value;
@@ -40,6 +119,8 @@ class EncryptPage extends Component {
         // To print or store the binary data, you may convert it to hex
         var encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
         console.log(encryptedHex);
+        encryptedLabel.value = encryptedHex;
+
         // "a338eda3874ed884b6199150d36f49988c90f5c47fe7792b0cf8c7f77eeffd87
         //  ea145b73e82aefcf2076f881c88879e4e25b1d7b24ba2788"
 
@@ -63,17 +144,82 @@ class EncryptPage extends Component {
         //console.log(ctx);
         //console.log(imgd);
         //console.log(pixelArray);
+        /*
+        var imageLoader = document.getElementById('encryptImageInput');
+        
 
+        var canvas = document.getElementById('imageCanvas');
+        var ctx = canvas.getContext('2d');
+        var messageInput = document.getElementById('message');
+
+        var reader = new FileReader();
+        */
+
+       
+   
+   
+   //handle decoding
+   //var decodeCanvas = document.getElementById('imageCanvas2');
+   //var dctx = decodeCanvas.getContext('2d');
+   //var imageLoader2 = document.getElementById('imageLoader2');
+       //imageLoader2.addEventListener('change', handleImage2, false);
+   
+   
+   /*
+   function handleImage2(e){
+       console.log('handle image 2');
+       var reader2 = new FileReader();
+       reader2.onload = function(event){
+           console.log('reader2 loaded');
+           var img2 = new Image();
+           img2.onload = function(){
+               console.log('img2 loaded');
+               decodeCanvas.width = img2.width;
+               decodeCanvas.height = img2.height;
+               dctx.drawImage(img2,0,0);
+               var decodeData = dctx.getImageData(0, 0, decodeCanvas.width, decodeCanvas.height);
+               for (var i = 0; i < decodeData.data.length; i += 4) {
+                   if (decodeData.data[i+1] % 10 == 7) {
+                       decodeData.data[i] = 0;
+                       decodeData.data[i+1] = 0;
+                       decodeData.data[i+2] = 0;
+                       decodeData.data[i+3] = 255;
+                   }
+                   else {
+                       decodeData.data[i+3] = 0;
+                   }
+               }
+               dctx.putImageData(decodeData, 0, 0);
+           };
+           img2.src = event.target.result;
+       };
+       reader2.readAsDataURL(e.target.files[0]);
+   }
+   */
+
+
+        
+
+    }
+
+    
+
+    wrapperFunction = () => {
+        this.handleImage();
+        this.encryptText();
+        
     }
 
     render() {
         return(
+            
             <div>
+                
                 <Header />
                 
                 <label id="encryptTextLabel">Text to be encryped into image:</label>
 
-                <input id="encryptTextInput" type="text" placeholder="Text to be encrypted"></input>
+                <input id="message" type="text" placeholder="Text to be encrypted"></input>
 
                 <label id="encryptKeyLabel">Key of 16 characters:</label>
 
@@ -81,9 +227,14 @@ class EncryptPage extends Component {
 
                 <label id="encryptImageLabel">Image to be encrypted:</label>
 
-                <input id="encryptImageInput" type="file"></input>
+                <input id="encryptedLabel" hidden disabled ></input>
 
-                <Button id="encryptButton" onClick={this.encryptText}>Encrypt</Button>
+                <input id="imageLoader" type="file"></input>
+
+                <canvas id="imageCanvas"></canvas>
+	            <canvas id="textCanvas"></canvas>
+
+                <Button id="encryptButton" onClick={this.wrapperFunction}>Encrypt</Button>
             </div>
         );
         
