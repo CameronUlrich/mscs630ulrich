c4ebaff8112bbf28becfe5687e04a6f0c1201836
CameronUlrich <Cameron.Ulrich1@marist.edu>
2020-05-07 14:46:46 -0400

    the canvas was somehow covering the decrypt key so I moved it because changing the size of the canvas was messing with it's scaling

--- prj/mscs630project/src/components/EncryptPage.jsx
+++ prj/mscs630project/src/components/EncryptPage.jsx
@@ -158,7 +158,7 @@ class EncryptPage extends Component {
 
                 <input id="encryptedLabel" hidden disabled ></input>
 
-                <input id="imageLoader" type="file" accept="image/png, image/jpeg" onClick={this.handleImage}></input>
+                <input id="imageLoader" type="file" accept="image/png, image/jpeg" onClick={this.wrapperFunction}></input>
 
                 <canvas id="imageCanvas"></canvas>
 	            <canvas id="textCanvas" hidden></canvas>
